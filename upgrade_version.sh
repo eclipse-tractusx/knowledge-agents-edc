b4ac6a999502efbf81cd8162f40241ff12feb917
Dr. Christoph "Schorsch" Jung <c-jung@t-systems.com>
2023-11-14 13:31:57 +0100

    chore: adds a version upgrade script

--- upgrade_version.sh
+++ upgrade_version.sh
@@ -1,4 +1,4 @@
 echo Upgrading from 1.10.6-SNAPSHOT to $1
 PATTERN=s/1.10.6-SNAPSHOT/$1/g
 LC_ALL=C
-find ./ -type f -name "pom.xml' -exec sed -i.bak $PATTERN {} \;
+find ./ -type f -regex *.(xml|sh|yml|yaml|md|java|properties) -exec sed -i.bak $PATTERN {} \;
